0a075f7dc0090717f3e00fb20e6ab0f72b691428
Michael Clark <michaeljclark@mac.com>
2022-03-21 13:59:43 +1300

    tests: template multiple type widths to increase test coverage

--- test/codegen_atomics.c
+++ test/codegen_atomics.c
@@ -1,32 +1,75 @@
 #include <stdatomic.h>
 
-/* generates atomic_int. needs to be templated for other types. */
+typedef long long llong;
 
-_Atomic int v;
+atomic_char v_char;
+atomic_short v_short;
+atomic_int v_int;
+atomic_long v_long;
+atomic_llong v_llong;
+
 atomic_flag f = ATOMIC_FLAG_INIT;
 
-int f1() {
-    return atomic_exchange((_Atomic int*)&v,5);
-}
-_Bool f2() {
-    int n = 0;
-    return atomic_compare_exchange_strong((_Atomic int*)&v,&n,5);
-}
-int f3() {
-    return atomic_fetch_add((_Atomic int*)&v,5);
-}
-int f4() {
-    return atomic_fetch_sub((_Atomic int*)&v,5);
-}
-int f5() {
-    return atomic_fetch_and((_Atomic int*)&v,5);
-}
-int f6() {
-    return atomic_fetch_or((_Atomic int*)&v,5);
-}
-int f7() {
-    return atomic_fetch_xor((_Atomic int*)&v,5);
-}
-int f8() {
+#define concat2(a,b) a ## _ ## b
+#define concat3(a,b,c) a ## _ ## b ## _ ## c
+
+#define gen_atomic_exchange(type) type concat2(test_atomic_exchange,type)() \
+    { return atomic_exchange((_Atomic type*)&concat2(v,type),5); }
+
+#define gen_atomic_compare_exchange(type) _Bool concat2(test_atomic_compare_exchange,type)() \
+    { type n = 0; return atomic_compare_exchange_strong((_Atomic type*)&concat2(v,type),&n,5); }
+
+#define gen_atomic_fetch_op(op,type) type concat3(test_atomic_fetch,op,type)() \
+    { return concat2(atomic_fetch,op)((_Atomic type*)&concat2(v,type),5); }
+
+gen_atomic_exchange(char)
+gen_atomic_exchange(short)
+gen_atomic_exchange(int)
+gen_atomic_exchange(long)
+gen_atomic_exchange(llong)
+
+gen_atomic_compare_exchange(char)
+gen_atomic_compare_exchange(short)
+gen_atomic_compare_exchange(int)
+gen_atomic_compare_exchange(long)
+gen_atomic_compare_exchange(llong)
+
+gen_atomic_fetch_op(add,char)
+gen_atomic_fetch_op(add,short)
+gen_atomic_fetch_op(add,int)
+gen_atomic_fetch_op(add,long)
+gen_atomic_fetch_op(add,llong)
+
+gen_atomic_fetch_op(sub,char)
+gen_atomic_fetch_op(sub,short)
+gen_atomic_fetch_op(sub,int)
+gen_atomic_fetch_op(sub,long)
+gen_atomic_fetch_op(sub,llong)
+
+gen_atomic_fetch_op(and,char)
+gen_atomic_fetch_op(and,short)
+gen_atomic_fetch_op(and,int)
+gen_atomic_fetch_op(and,long)
+gen_atomic_fetch_op(and,llong)
+
+gen_atomic_fetch_op(or,char)
+gen_atomic_fetch_op(or,short)
+gen_atomic_fetch_op(or,int)
+gen_atomic_fetch_op(or,long)
+gen_atomic_fetch_op(or,llong)
+
+gen_atomic_fetch_op(xor,char)
+gen_atomic_fetch_op(xor,short)
+gen_atomic_fetch_op(xor,int)
+gen_atomic_fetch_op(xor,long)
+gen_atomic_fetch_op(xor,llong)
+
+int test_atomic_flag_test_and_set()
+{
     return atomic_flag_test_and_set(&f);
+}
+
+void test_atomic_flag_clear()
+{
+    atomic_flag_clear(&f);
 }
--- test/test_atomics.c
+++ test/test_atomics.c
@@ -2,64 +2,163 @@
 #include <assert.h>
 #include <stdatomic.h>
 
-/* tests atomic_int. needs to be templated for other types. */
+typedef long long llong;
 
-void t1() {
-    _Atomic int v = 0;
-    assert(atomic_exchange((_Atomic int*)&v,5) == 0);
-    assert(atomic_exchange((_Atomic int*)&v,0) == 5);
-}
-void t2() {
-    _Atomic int v = 0;
-    int o = 0;
-    assert(atomic_compare_exchange_strong((_Atomic int*)&v,&o,5));
-    assert(!atomic_compare_exchange_strong((_Atomic int*)&v,&o,5));
-}
-void t3() {
-    _Atomic int v = 0;
-    assert(atomic_fetch_add((_Atomic int*)&v,5) == 0);
-    assert(atomic_fetch_add((_Atomic int*)&v,5) == 5);
-}
-void t4() {
-    _Atomic int v = 0;
-    assert(atomic_fetch_sub((_Atomic int*)&v,5) == 0);
-    assert(atomic_fetch_sub((_Atomic int*)&v,5) == -5);
-}
-void t5() {
-    _Atomic int v = 0;
-    assert(atomic_fetch_and((_Atomic int*)&v,5) == 0);
-    assert(atomic_fetch_and((_Atomic int*)&v,5) == 0);
-}
-void t6() {
-    _Atomic int v = 0;
-    assert(atomic_fetch_or((_Atomic int*)&v,5) == 0);
-    assert(atomic_fetch_or((_Atomic int*)&v,5) == 5);
-}
-void t7() {
-    _Atomic int v = 0;
-    assert(atomic_fetch_xor((_Atomic int*)&v,5) == 0);
-    assert(atomic_fetch_xor((_Atomic int*)&v,5) == 5);
-}
-void t8() {
+#define concat2(a,b) a ## _ ## b
+#define concat3(a,b,c) a ## _ ## b ## _ ## c
+
+#define gen_atomic_exchange(type) void concat2(test_atomic_exchange,type)() { \
+    _Atomic type v = 0; \
+    assert(atomic_exchange((_Atomic type*)&v,5) == 0); \
+    assert(atomic_exchange((_Atomic type*)&v,0) == 5); }
+
+#define gen_atomic_compare_exchange(type) void concat2(test_atomic_compare_exchange,type)() { \
+    _Atomic type v = 0; type o = 0; \
+    assert(atomic_compare_exchange_strong((_Atomic type*)&v,&o,5)); \
+    assert(!atomic_compare_exchange_strong((_Atomic type*)&v,&o,5)); }
+
+#define gen_atomic_fetch_add(type) void concat2(test_atomic_fetch_add,type)() { \
+    _Atomic type v = 0; \
+    assert(atomic_fetch_add((_Atomic type*)&v,5) == 0); \
+    assert(atomic_fetch_add((_Atomic type*)&v,5) == 5); }
+
+#define gen_atomic_fetch_sub(type) void concat2(test_atomic_fetch_sub,type)() { \
+    _Atomic type v = 0; \
+    assert(atomic_fetch_sub((_Atomic type*)&v,5) == 0); \
+    assert(atomic_fetch_sub((_Atomic type*)&v,5) == -5); }
+
+#define gen_atomic_fetch_and(type) void concat2(test_atomic_fetch_and,type)() { \
+    _Atomic type v = 0; \
+    assert(atomic_fetch_and((_Atomic type*)&v,5) == 0); \
+    assert(atomic_fetch_and((_Atomic type*)&v,5) == 0); }
+
+#define gen_atomic_fetch_or(type) void concat2(test_atomic_fetch_or,type)() { \
+    _Atomic type v = 0; \
+    assert(atomic_fetch_or((_Atomic type*)&v,5) == 0); \
+    assert(atomic_fetch_or((_Atomic type*)&v,5) == 5); }
+
+#define gen_atomic_fetch_xor(type) void concat2(test_atomic_fetch_xor,type)() { \
+    _Atomic type v = 0; \
+    assert(atomic_fetch_xor((_Atomic type*)&v,5) == 0); \
+    assert(atomic_fetch_xor((_Atomic type*)&v,5) == 5); }
+
+#define gen_atomic_is_lock_free(type) void concat2(test_atomic_is_lock_free,type)() \
+    { _Atomic type x = 0; assert(atomic_is_lock_free(&x)); }
+
+gen_atomic_exchange(char)
+gen_atomic_exchange(short)
+gen_atomic_exchange(int)
+gen_atomic_exchange(long)
+gen_atomic_exchange(llong)
+
+gen_atomic_compare_exchange(char)
+gen_atomic_compare_exchange(short)
+gen_atomic_compare_exchange(int)
+gen_atomic_compare_exchange(long)
+gen_atomic_compare_exchange(llong)
+
+gen_atomic_fetch_add(char)
+gen_atomic_fetch_add(short)
+gen_atomic_fetch_add(int)
+gen_atomic_fetch_add(long)
+gen_atomic_fetch_add(llong)
+
+gen_atomic_fetch_sub(char)
+gen_atomic_fetch_sub(short)
+gen_atomic_fetch_sub(int)
+gen_atomic_fetch_sub(long)
+gen_atomic_fetch_sub(llong)
+
+gen_atomic_fetch_and(char)
+gen_atomic_fetch_and(short)
+gen_atomic_fetch_and(int)
+gen_atomic_fetch_and(long)
+gen_atomic_fetch_and(llong)
+
+gen_atomic_fetch_or(char)
+gen_atomic_fetch_or(short)
+gen_atomic_fetch_or(int)
+gen_atomic_fetch_or(long)
+gen_atomic_fetch_or(llong)
+
+gen_atomic_fetch_xor(char)
+gen_atomic_fetch_xor(short)
+gen_atomic_fetch_xor(int)
+gen_atomic_fetch_xor(long)
+gen_atomic_fetch_xor(llong)
+
+gen_atomic_is_lock_free(char)
+gen_atomic_is_lock_free(short)
+gen_atomic_is_lock_free(int)
+gen_atomic_is_lock_free(long)
+gen_atomic_is_lock_free(llong)
+
+void test_atomic_flag_test_and_set()
+{
     atomic_flag f = ATOMIC_FLAG_INIT;
     assert(atomic_flag_test_and_set(&f) == 0);
     assert(atomic_flag_test_and_set(&f) == 1);
     atomic_flag_clear(&f);
 }
-void t9() {
-    _Atomic int x = 0;
-    assert(atomic_is_lock_free(&x));
+
+void test_atomic_flag_clear()
+{
+    atomic_flag f = ATOMIC_FLAG_INIT;
+    assert(atomic_flag_test_and_set(&f) == 0);
+    atomic_flag_clear(&f);
+    assert(atomic_flag_test_and_set(&f) == 0);
 }
 
 int main(int argc, const char **argv)
 {
-    t1();
-    t2();
-    t2();
-    t4();
-    t5();
-    t6();
-    t7();
-    t8();
-    t9();
+    test_atomic_exchange_char();
+    test_atomic_exchange_short();
+    test_atomic_exchange_int();
+    test_atomic_exchange_long();
+    test_atomic_exchange_llong();
+
+    test_atomic_compare_exchange_char();
+    test_atomic_compare_exchange_short();
+    test_atomic_compare_exchange_int();
+    test_atomic_compare_exchange_long();
+    test_atomic_compare_exchange_llong();
+
+    test_atomic_fetch_add_char();
+    test_atomic_fetch_add_short();
+    test_atomic_fetch_add_int();
+    test_atomic_fetch_add_long();
+    test_atomic_fetch_add_llong();
+
+    test_atomic_fetch_sub_char();
+    test_atomic_fetch_sub_short();
+    test_atomic_fetch_sub_int();
+    test_atomic_fetch_sub_long();
+    test_atomic_fetch_sub_llong();
+
+    test_atomic_fetch_and_char();
+    test_atomic_fetch_and_short();
+    test_atomic_fetch_and_int();
+    test_atomic_fetch_and_long();
+    test_atomic_fetch_and_llong();
+
+    test_atomic_fetch_or_char();
+    test_atomic_fetch_or_short();
+    test_atomic_fetch_or_int();
+    test_atomic_fetch_or_long();
+    test_atomic_fetch_or_llong();
+
+    test_atomic_fetch_xor_char();
+    test_atomic_fetch_xor_short();
+    test_atomic_fetch_xor_int();
+    test_atomic_fetch_xor_long();
+    test_atomic_fetch_xor_llong();
+
+    test_atomic_is_lock_free_char();
+    test_atomic_is_lock_free_short();
+    test_atomic_is_lock_free_int();
+    test_atomic_is_lock_free_long();
+    test_atomic_is_lock_free_llong();
+
+    test_atomic_flag_test_and_set();
+    test_atomic_flag_clear();
 }
